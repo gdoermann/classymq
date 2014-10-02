79a8b140ebea30ce61c2de23cdf216ebe56991bc
Greg Doermann <gdoermann@gmail.com>
2014-10-02 01:13:22 -0600

    Stopped reconnecting for test factory.

--- classymq/tests/base.py
+++ classymq/tests/base.py
@@ -11,6 +11,17 @@ TEST_RABBIT_MQ_PORT = resolve_setting('TEST_RABBIT_MQ_PORT', 5672)
 
 TEST_VHOST = resolve_setting('TEST_VHOST', '/')
 
+class TestFactory(factory.AmqpFactory):
+    def clientConnectionFailed(self, connector, reason):
+        self.continueTrying = False
+        factory.AmqpFactory.clientConnectionFailed(self, connector, reason)
+        raise RuntimeError('AMQP connection failed')
+
+    def clientConnectionLost(self, connector, reason):
+        self.continueTrying = False
+        factory.AmqpFactory.clientConnectionFailed(self, connector, reason)
+        raise RuntimeError('Lost AMQP connection')
+
 
 class ClassyTestCase(TestCase):
 
@@ -22,7 +33,7 @@ class ClassyTestCase(TestCase):
         super(ClassyTestCase, self).setUp()
 
     def get_factory(self):
-        con = factory.AmqpFactory(vhost=TEST_VHOST, host=TEST_RABBIT_MQ_HOST, port=TEST_RABBIT_MQ_PORT)
+        con = TestFactory(vhost=TEST_VHOST, host=TEST_RABBIT_MQ_HOST, port=TEST_RABBIT_MQ_PORT)
         self.defer_until_connected = con.connect()
         return con
 
